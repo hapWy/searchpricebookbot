2dbf9c0959c2dd503b48a0184f9b930a86d08c53
hapWy <112803897+hapWy@users.noreply.github.com>
2022-09-04 15:01:57 +0300

    Delete req.py

--- scrap/req.py
+++
@@ -1,9 +0,0 @@
-import csv
-from aiogram import types
-
-def grableRequest(message: types.Message):
-    with open('requests.csv','a', newline='') as file:
-        csv.writer(file, delimiter=';').writerow((message.from_user.username, message.text))
-    return message.text
-
-
