2e73e8b9661e02c1d643fce097b6637218f48805
hapWy <112803897+hapWy@users.noreply.github.com>
2022-09-09 17:05:59 +0300

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -1,4 +1,5 @@
 from aiogram import types, Dispatcher
+from aiogram.utils.markdown import hbold, hlink
 from create_bot import dp, bot
 from keyboard import kb_client
 from scrap import scrapPrice
@@ -6,11 +7,11 @@ from scrap import scrapPrice
 
 
 async def start_command(message : types.Message):
-    await message.answer('Привет,\nЯ могу искать книги на сайтах (пока только трёх) и выводить самый дешёвый вариант с каждого сайта'
+    await message.answer('Привет,\nЯ могу искать книги на сайтах ( пока только трёх ) и выводить самый дешёвый вариант с каждого сайта'
                          , reply_markup=kb_client)
 
 async def help_command(message: types.Message):
-    await message.answer('Функций тут не особо много, так что помогать и особо не с чем, но могу рассказать что я ищу самую дешёвую книгу по введёным вами данными, поэтому поиск не самый точный :\\ \nИ чем менее точный ваш запрос тем дольше ваш прийдётся ждать \nТакже, если вам интресно кто меня сделал введите - /contact ')
+    await message.answer('Функций тут не особо много, так что помогать и особо не с чем, но могу рассказать что я ищу самый дешёвый вариант по введёным вами данными, поэтому поиск не самый точный :\\ \n Также, если вам интресно кто меня сделал введите - /contact ')
 
 
 async def contact(message: types.Message):
@@ -21,17 +22,24 @@ async def contact(message: types.Message):
 
 async def all_commands(message : types.Message):
     if message.text == 'Поиск книги':
-        await message.answer('Введите запрос (Выводится только одна книга с одного сайта (Подробнее - /help))')
+        await message.answer('Введите запрос( Выводится только одна книга с одного сайта( Подробнее - /help ))')
     else:
         msd = await message.answer('Запрос обрабатывается. Пожалуйста, подождите')
-        print(f'{message.from_user.username} - {message.text}')
         answ = scrapPrice(message.text)
         await msd.delete()
         if answ == []:
-            await message.answer('Ничего не найдено :(\nПопробуйте что-нибудь другое')
+            await message.answer('Ничего не найдено :(\nПопройбуйте что-нибудь другое')
         for i in answ:
             try:
-                await message.answer_photo(i['picture'] ,caption=f"Сайт: {i['site']}\nНазвание: {i['name']}\nАвтор: {i['author']}\nЦена: {i['price']}\nISBN: {i['ISBN']}\nСсылка: {i['url']} ")
+                await message.answer_photo(i['picture'] ,
+                                           caption=f"{hbold('Сайт:')} {i['site']}"
+                                           f"\n{hbold('Книга:')} {hlink(i['name'], i['url'])}"
+                                            f"\n{hbold('Автор:')} {i['author']}\n{hbold('Цена:')} {i['price']}"
+                                            f"\n{hbold('ISBN:')} {i['ISBN']}")
+
+
+
+
             except Exception as ex:
                 await message.answer(f'Ошибка...\nПропустим этот сайт')
                 continue
